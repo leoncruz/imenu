1f02839c941fd54575edfe85317532a2e2034f85
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-09-13 23:58:22 -0300

    feat: add new css animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,23 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
     './app/components/**/*.html.erb'
-  ]
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'slide-in': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' }
+        },
+        'slide-out': {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(100%)' }
+        }
+      },
+      animation: {
+        'slide-in': 'slide-in 0.3s ease-in',
+        'slide-out': 'slide-out 0.3s ease-out'
+      }
+    }
+  }
 }
